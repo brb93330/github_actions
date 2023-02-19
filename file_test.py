440af2e9e9ada5fe551194b4087960132539c687
Bruno <brb93330@gmail.com>
2023-02-19 01:15:47 +0000

    test4

--- file_test.py
+++ file_test.py
@@ -2,8 +2,8 @@ import pytest
 
 def test_calc_addition():
   # Fonction test du résultat de 2+4
-    output = 2+8
-    assert output == 10
+    output = 2+9
+    assert output == 11
 
 def test_calc_substraction():
   # Fonction test du résultat de 2-4
